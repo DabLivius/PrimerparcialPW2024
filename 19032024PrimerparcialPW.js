480a20f1c9eb848e77311e7160a6592c41663498
DABLivius <liviustd@gmail.com>
2024-03-19 10:29:35 -0500

    Add login functionality and update CSS styles

---
+++ 19032024PrimerparcialPW.js
@@ -0,0 +1,6 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  console.log("Username: " + username);
+  console.log("Password: " + password);
+}
